96b34583f9768ea2fed17df64179ec7e239491f9
LuisCardenal <luis@rocktdigital.com>
2022-11-14 14:15:28 -0600

    solo modulo de prestamos

--- localization_sv/models/account_journal_sv.py
+++
@@ -1,9 +0,0 @@
-from odoo import models, fields, api
-
-
-class AccountJournal(models.Model):
-    _inherit = 'account.journal'
-
-    num_correlativo = fields.Char(string='Número de correlativo: ', readonly=True, default=1)
-
-
